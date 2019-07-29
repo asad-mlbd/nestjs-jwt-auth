77ae7d91c9d80cb1b1928da0213c4417ff97520d
Asad Rahman <asad.rahman@sekai-lab-bd.com>
2019-07-29 16:00:01 +0600

    feat(db): configure typeorm, add db config in environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : null,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      debug: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
